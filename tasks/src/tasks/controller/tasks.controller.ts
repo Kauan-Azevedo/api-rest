c8cc82d81a93cf4a25dc9ec83e3455bad414099a
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-06-06 18:31:39 -0300

    Add TaskController, TaskModel, and TaskService classes

---
+++ tasks/src/tasks/controller/tasks.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import TaskService from "../services/tasks.service";
+
+
+export default class TaskController {
+    constructor(private taskService: TaskService) {}
+
+    async create(req: Request, res: Response) {
+        const task = await this.taskService.create(req.body);
+
+        return res.json(task);
+    }
+
+    async getAll(req: Request, res: Response) {
+        const tasks = await this.taskService.getAll();
+
+        return res.json(tasks);
+    }
+
+    async getById(req: Request, res: Response) {
+        const { id } = req.params;
+        const task = await this.taskService.getById(Number(id));
+
+        return res.json(task);
+    }
+
+    async update(req: Request, res: Response) {
+        const { id } = req.params;
+        const task = await this.taskService.update(Number(id), req.body);
+
+        return res.json(task);
+    }
+
+    async delete(req: Request, res: Response) {
+        const { id } = req.params;
+        await this.taskService.delete(Number(id));
+
+        return res.status(204).send();
+    }
+}
---
+++ tasks/src/tasks/services/tasks.service.ts
@@ -0,0 +1,23 @@
+import { Task } from "../model/tasks.model";
+
+export default class TaskService {
+    create(task: Omit<string, any>) {
+        return Task.create(task);
+    }
+
+    getAll() {
+        return Task.findAll();
+    }
+
+    getById(id: number) {
+        return Task.findByPk(id);
+    }
+
+    update(id: number, task: Omit<string, any>) {
+        return Task.update(task, { where: { id } });
+    }
+
+    delete(id: number) {
+        return Task.destroy({ where: { id } });
+    }
+}
