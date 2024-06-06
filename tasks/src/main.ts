d2f2b50d0b5458a58a49cd78f560c7ea996a8cde
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-06-06 18:31:30 -0300

    Add tasks API endpoints

---
+++ tasks/src/main.ts
@@ -0,0 +1,55 @@
+import express, { Request, Response } from 'express';
+import { Task } from './tasks/model/tasks.model';
+import { sequelize } from './db/db.config';
+import bodyParser from 'body-parser';
+import cors from "cors";
+
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+sequelize.sync().then(() => {
+    console.log('Database & tables created!');
+});
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to the Tasks API!');
+});
+
+app.get('/tasks', async (req: Request, res: Response) => {
+    const tasks = await Task.findAll();
+    res.json(tasks);
+});
+
+app.post('/tasks', async (req: Request, res: Response) => {
+    const task = await Task.create(req.body);
+    res.json(task);
+});
+
+app.get('/tasks/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const task = await Task.findByPk(id);
+    res.json(task);
+});
+
+app.put('/tasks/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const task = await Task.update(req.body, { where: { id } });
+    res.json(task);
+});
+
+app.delete('/tasks/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    await Task.destroy({ where: { id } });
+    res.status(204).send();
+});
+
+app.listen(port, () => {
+    console.log(`🚀🚀 Server is running at http://localhost:${port} 🚀🚀`);
+});
