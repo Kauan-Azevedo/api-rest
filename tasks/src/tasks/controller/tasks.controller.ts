b51e30e4868dc9199eb2ca5104121f45ac7a5604
Kauan-Azevedo <kauanazevedo2010@gmail.com>
2024-06-06 18:46:03 -0300

    Add cors dependency and handle error responses in task controller

--- tasks/src/tasks/controller/tasks.controller.ts
+++ tasks/src/tasks/controller/tasks.controller.ts
@@ -8,33 +8,49 @@ export default class TaskController {
     async create(req: Request, res: Response) {
         const task = await this.taskService.create(req.body);
 
-        return res.json(task);
+        return res.status(201).json(task);
     }
 
     async getAll(req: Request, res: Response) {
-        const tasks = await this.taskService.getAll();
-
-        return res.json(tasks);
+        try {
+            const tasks = await this.taskService.getAll();
+    
+            return res.status(200).json(tasks);
+        } catch (error: any) {
+            return res.status(500).json({ message: error.message });
+        }
     }
 
     async getById(req: Request, res: Response) {
-        const { id } = req.params;
-        const task = await this.taskService.getById(Number(id));
-
-        return res.json(task);
+        try {
+            const { id } = req.params;
+            const task = await this.taskService.getById(Number(id));
+    
+            return res.status(200).json(task);
+        } catch (error: any) {
+            return res.status(500).json({ message: error.message });
+        }
     }
 
     async update(req: Request, res: Response) {
-        const { id } = req.params;
-        const task = await this.taskService.update(Number(id), req.body);
-
-        return res.json(task);
+        try {
+            const { id } = req.params;
+            const task = await this.taskService.update(Number(id), req.body);
+    
+            return res.status(200).json(task);
+        } catch(error: any ) {
+            return res.status(500).json({ message: error.message });
+        }
     }
 
     async delete(req: Request, res: Response) {
-        const { id } = req.params;
-        await this.taskService.delete(Number(id));
-
-        return res.status(204).send();
+        try {
+            const { id } = req.params;
+            await this.taskService.delete(Number(id));
+    
+            return res.status(204).send();
+        } catch (error: any) {
+            return res.status(500).json({ message: error.message })
+        }
     }
 }
--- tasks/src/tasks/model/tasks.model.ts
+++ tasks/src/tasks/model/tasks.model.ts
@@ -1,5 +1,5 @@
 import { DataTypes, Model } from "sequelize";
-import { sequelize } from "../db/db.config";
+import { sequelize } from "../../db/db.config";
 
 class Task extends Model {}
 
